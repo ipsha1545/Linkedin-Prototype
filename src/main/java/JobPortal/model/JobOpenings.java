9f97efa71db77d4d9e031c8126ff0a853cf79a1a
sandhya123r <sandhyafeb1990@gmail.com>
2017-05-03 02:00:12 +0000

    Add JobOpenings data model

---
+++ src/main/java/JobPortal/model/JobOpenings.java
@@ -0,0 +1,100 @@
+package JobPortal.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="job_openings")
+public class JobOpenings {
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "jobId")
+    private int jobId;
+    
+    @Column(name = "companyId")
+    private int companyId;
+
+    @Column(name = "title")
+    private String title;
+    
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "responsibilities")
+    private String responsibilities;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @Column(name = "companyname")
+    private String companyname;
+   
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    } 
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCompanyname() {
+        this.companyname = companyname;
+    }
+    
+    public String getCompanyname() {
+           return companyname;
+    }
+
+
+    public String getResponsibilities() {
+        return responsibilities;
+    }
+
+    public void setResponsibilities(String responsibilities) {
+        this.responsibilities = responsibilities;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    
+}
