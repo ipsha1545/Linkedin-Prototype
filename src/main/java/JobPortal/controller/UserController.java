0975411bdbfc06e0b26265d643a367b18d9801e4
anvib <anvita.bathija@sjsu.edu>
2017-05-02 20:13:28 -0700

    user verification + update profile added - anvita

--- src/main/java/JobPortal/controller/UserController.java
+++ src/main/java/JobPortal/controller/UserController.java
@@ -2,6 +2,7 @@ package JobPortal.controller;
 
 import JobPortal.model.User;
 import JobPortal.service.UserService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
 
 
 @Controller
@@ -20,39 +24,37 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value= "/users/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Object> getById(HttpServletResponse response, @PathVariable(value="id") int id) {
+    @RequestMapping(value= "/users", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity getById(HttpServletResponse response,
+                                          @RequestParam(value="email", required = true) String email,
+                                          @RequestParam(value="password", required = true) String password) {
 
 
-        User user;
+        User user = userService.getUser(email);
 
-        ModelAndView modelAndView = null;
-        ModelMap model = new ModelMap();
-
-
-            user = userService.getUser(id);
-
-        model.addAttribute("id", user.getUserId());
-        model.addAttribute("firstname", user.getFirstname());
-        model.addAttribute("lastname", user.getLastname());
-        model.addAttribute("email", user.getEmail());
-        model.addAttribute("education", user.getEducation());
-        model.addAttribute("image", user.getImage());
-        model.addAttribute("introduction", user.getIntroduction());
-        model.addAttribute("skills", user.getSkills());
-        model.addAttribute("status", user.getStatus());
-        model.addAttribute("experience", user.getExperience());
-
-        return ResponseEntity.ok(model);
-
+        return new ResponseEntity<>(user, new HttpHeaders(), HttpStatus.OK);
 
     }
 
 
+    @RequestMapping(value= "/users/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Object> getById(HttpServletResponse response, @PathVariable(value="id") String id) {
+
+
+        User user;
+        ModelMap model = new ModelMap();
+
+        user = userService.getUserByID(Integer.valueOf(id));
+
+        return ResponseEntity.ok(user);
+
+    }
+
     @RequestMapping(value= "/users", method = RequestMethod.POST)
-    public ResponseEntity<Object> create(HttpServletResponse response,@RequestParam(value="firstname", required = true) String firstname,
+    public ResponseEntity create(HttpServletResponse response,@RequestParam(value="firstname", required = true) String firstname,
                                          @RequestParam(value="lastname", required = true) String lastname,
                                          @RequestParam(value="email", required = true) String email,
+                                         @RequestParam(value="password", required = true) String password,
                                          @RequestParam(value="image", required = false) String image,
                                          @RequestParam(value="introduction", required = false) String introduction,
                                          @RequestParam(value="experience", required = true) Float experience,
@@ -65,10 +67,58 @@ public class UserController {
 
     ) throws IOException {
 
-        ResponseEntity<Object> rs = userService.createUser(firstname, lastname, phone, email, address, education, skills, experience, introduction, status, image);
+        return userService.createUser(firstname, lastname, phone, email, password, address, education, skills, experience, introduction, status, image);
+    }
+
+    @RequestMapping(value= "/users/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> update(HttpServletResponse response,
+                                         @PathVariable(value="id") String id,
+                                         @RequestParam Map<String,String> params
+
+
+    ) throws IOException {
+
+        ResponseEntity<Object> rs = null;
+
+        if(params.get("firstname") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "firstname", params.get("firstname"));
+        }
+
+        if(params.get("lastname") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "lastname", params.get("lastname"));
+        }
+
+        if(params.get("address") !=null){
+
+            rs = userService.updateUser(Integer.valueOf(id), "address", params.get("address"));
+        }
+
+        if(params.get("education") !=null){
+
+            rs = userService.updateUser(Integer.valueOf(id), "education", params.get("education"));
+        }
+
+        if(params.get("introduction") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "introduction", params.get("introduction"));
+        }
+
+        if(params.get("phone") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "phone", params.get("phone"));
+        }
+
+        if(params.get("skills") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "skills", params.get("skills"));
+        }
+
+        if(params.get("status") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "skills", params.get("status"));
+        }
+
+        if(params.get("verified") !=null){
+            rs = userService.updateUser(Integer.valueOf(id), "verified", params.get("verified"));
+        }
 
         return rs;
-
     }
 
 
--- src/main/java/JobPortal/service/UserService.java
+++ src/main/java/JobPortal/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.http.ResponseEntity;
 import javax.transaction.Transactional;
+import java.util.Random;
 
 /**
  * Created by anvita on 4/28/17.
@@ -21,7 +22,7 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
-    public UserService(){
+    public UserService() {
 
     }
 
@@ -29,51 +30,156 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public User getUser(int id){
+    public User getUser(String email) {
+        return userDao.findUserByEmail(email);
+    }
+
+    public User getUserByID(int id) {
         return userDao.findByuserId(id);
     }
 
 
-    public ResponseEntity<Object> createUser(String firstname, String lastname, String phone, String email, String address, String education, String skills,
-                                             Float experience, String introduction, String status, String image){
-
-        ModelMap model = new ModelMap();
-        int id;
+    public ResponseEntity createUser(String firstname, String lastname, String phone, String email, String password,
+                                             String address, String education, String skills, Float experience,
+                                             String introduction, String status, String image) {
 
         User user;
 
-        try{
+        try {
             User userPhone = userDao.findUserByPhone(phone);
             User userEmail = userDao.findUserByEmail(email);
 
             System.out.println("request recieved in user service");
 
-            if(userEmail == null){
-                if(userPhone == null){
-                    user = new User(firstname, lastname, phone, email, address, education, skills,experience, introduction, status, image);
+            if (userEmail == null) {
+                if (userPhone == null) {
+
+                    Random rand = new Random();
+                    String random_verification =
+                            "" + (rand.nextInt(9) + 1) + "" + (rand.nextInt(9) + 1) + ""
+                                    + (rand.nextInt(9) + 1) + "" + (rand.nextInt(9) + 1) + "" + (rand.nextInt(9) + 1);
+                    System.out.println(random_verification);
+                    int verification = Integer.valueOf(random_verification);
+
+                    user = new User(firstname, lastname, phone, email, password, address, education, skills, experience, introduction, status, image, verification);
                     userDao.save(user);
 
-                    id = user.getUserId();
-                    model.addAttribute("user", id);
+                    ModelMap model = new ModelMap();
+                    model.addAttribute("code", 200);
+                    model.addAttribute("user", user);;
+                    return ResponseEntity.ok(model);
 
-                    return  ResponseEntity.ok(model);
-
-                }else{
-                    System.out.println("phone exists - creating user");
-                    model.addAttribute("user", -1);
-                    return  ResponseEntity.ok(model);
+                } else {
+                    ModelMap model = new ModelMap();
+                    model.addAttribute("code", 400);
+                    model.addAttribute("msg", "Phone number already exists");
+                    return ResponseEntity.ok(model);
                 }
-            }else{
-                model.addAttribute("user", 0);
-                System.out.println("email exists - creating user");
-                return  ResponseEntity.ok(model);
+            } else {
+                ModelMap model = new ModelMap();
+                model.addAttribute("code", 400);
+                model.addAttribute("msg", "Email already exists");
+                return ResponseEntity.ok(model);
             }
-        }catch(Exception ex){
-            System.out.println("some unknown error creating user");
-            model.addAttribute("user", -2);
-            return  ResponseEntity.ok(model);
+        } catch (Exception ex) {
+            ModelMap model = new ModelMap();
+            model.addAttribute("code", 400);
+            model.addAttribute("msg", "Error creating user");
+            return ResponseEntity.ok(model);
         }
     }
 
+
+    public ResponseEntity<Object> updateUser(int id, String parameter, String value) {
+
+        ModelMap model = new ModelMap();
+
+        User user = userDao.findByuserId(id);
+
+        if(user != null){
+
+            try{
+                if(parameter.equals("firstname")){
+                    user.setFirstname(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("lastname")){
+                    user.setLastname(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("address")){
+                    user.setAddress(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("education")){
+                    user.setEducation(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("introduction")){
+                    user.setIntroduction(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("phone")){
+                    user.setPhone(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("skills")){
+                    user.setSkills(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("status")){
+                    user.setStatus(value);
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+                if(parameter.equals("verified")){
+                    user.setVerified(Integer.valueOf(value));
+                    userDao.save(user);
+
+                    model.addAttribute("statuscode",200);
+                    return ResponseEntity.ok(model);
+                }
+
+            }catch(Exception e){
+                model.addAttribute("statuscode",400);
+                return ResponseEntity.ok(model);
+            }
+
+        }
+
+return  null;
+
+    }
 
 }
