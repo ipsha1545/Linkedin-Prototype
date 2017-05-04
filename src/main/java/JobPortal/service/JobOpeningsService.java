1f027a5f12026c723560a9df5ee3311c5e344277
sandhya123r <sandhyafeb1990@gmail.com>
2017-05-04 00:42:57 +0000

    Add job openings

---
+++ src/main/java/JobPortal/service/JobOpeningsService.java
@@ -0,0 +1,23 @@
+package JobPortal.service;
+
+import JobPortal.Dao.JobOpeningsDao;
+
+import JobPortal.model.Company;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by ipshamohanty on 5/1/17.
+ */
+
+@Service
+@Transactional
+public class JobOpeningsService {
+    
+    
+}
