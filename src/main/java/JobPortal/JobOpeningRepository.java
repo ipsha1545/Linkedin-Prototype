d2a40a07418e3cfbbd80fdf3f78ab3eff7ce27c6
Ipsha Mohanty <ipsha1545@gmail.com>
2017-05-12 10:45:18 -0700

    Created interface JobOpeningRepository
    Created interface JobOpeningRepository to extend Crud Repository Methods

---
+++ src/main/java/JobPortal/JobOpeningRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.repository.CrudRepository;
+
+import JobPortal.model.JobOpening;
+
+public interface JobOpeningRepository extends CrudRepository<JobOpening, Integer>{
+}
