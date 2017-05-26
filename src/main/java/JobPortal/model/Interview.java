11eefc2a871f12de7a66056b8a6b4dec9365b57e
anvitabathija <bathija.anvita@gmail.com>
2017-05-25 19:31:35 -0700

    Update Interview.java

--- src/main/java/JobPortal/model/Interview.java
+++ src/main/java/JobPortal/model/Interview.java
@@ -9,39 +9,43 @@ import javax.persistence.*;
 @Entity
 @Table(name="interview")
 public class Interview {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    @Column(name = "interviewId")
-    private int interviewId;
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  @Column(name = "interviewId")
+  private int interviewId;
 
-    @Column
-    private int userId;
+  @Column
+  private int userId;
 
-    @Column
-    private int jobId;
+  @Column
+  private int jobId;
 
-    @Column
-    private String status;
+  @Column
+  private int applicationId;
 
-    @Column
-    private String location;
+  @Column
+  private String status;
 
-    @Column 
-    private String time;
+  @Column
+  private String location;
 
-    @Column
-    private String feedback;
+  @Column
+  private String time;
 
-    public Interview() {
-    
-    }
-    
-   public Interview(int userId, int jobId, String location, String time, String status) {
-        this.userId = userId;
-        this.jobId = jobId;
-        this.location = location;
-        this.time = time;
-        this.status = status;
-   }
-    
+  @Column
+  private String feedback;
+
+  public Interview() {
+
+  }
+
+  public Interview(int userId, int jobId, int applicationId, String location, String time, String status) {
+    this.userId = userId;
+    this.jobId = jobId;
+    this.applicationId = applicationId;
+    this.location = location;
+    this.time = time;
+    this.status = status;
+  }
+
 }
